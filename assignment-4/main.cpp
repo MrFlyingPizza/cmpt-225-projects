864bcb8ebf2d2980ed3556d2a6183ae58c29aec0
hga38 <richard_gao@sfu.ca>
2020-11-02 19:37:59 -0800

    implemented tests

--- assignment-4/main.cpp
+++ assignment-4/main.cpp
@@ -8,39 +8,80 @@
 
 using namespace std;
 
-void heapsort1()
+string entriesToString(Entry const* entries, const int n)
 {
-    
+    string st = "{ ";
+    for (int i = 0; i < n; ++i)
+    {
+        st.append(entries[i].toString());
+        if (i != n-1)
+            st.append(", ");
+    }
+    st.append(" }");
+    return st;
 }
 
-void heapsort2()
+Entry* heapsort1(Entry* entries, const int n)
 {
+    Heap heap;
+    for (int i = 0; i < n; ++i)
+        heap.insert(entries[i]);
 
+    for (int i = n-1; i >= 0; --i)
+        entries[i] = heap.removeMax();
+
+    return entries;
+}
+
+Entry* heapsort2(Entry* entries, const int n)
+{
+    Heap heap;
+    heap.make(entries, n);
+
+    for (int i = n-1; i >= 0; --i)
+        entries[i] = heap.removeMax();
+
+    return entries;
+}
+
+void test_heapsort1(const int n)
+{
+    Entry entries[n];
+    for (int i = 0; i < n; ++i)
+        entries[i].random();
+    cout << "unsorted: " << entriesToString(entries, n) << endl;
+
+    heapsort1(entries, n);
+
+    cout << "  sorted: " << entriesToString(entries, n) << endl;
+}
+
+void test_heapsort2(const int n)
+{
+    Entry entries[n];
+    for (int i = 0; i < n; ++i)
+        entries[i].random();
+    
+    cout << "unsorted: " << entriesToString(entries, n) << endl;
+
+    heapsort1(entries, n);
+
+    cout << "  sorted: " << entriesToString(entries, n) << endl;
 }
 
 int main()
 {
     srand(time(0));
-    Entry e = Entry();
-    Heap heap = Heap();
-    const int count = 15;
-    string out_st = "unsorted: {";
-    for (int i = 0; i < count; i++)
-    {
-        e.random();
-        out_st.append(e.toString());
-        heap.insert(e);
-        if (i != count-1) out_st.append(",");
-    }
-    out_st.append("}");
-    cout << out_st << endl;
-    
-    out_st = "sorted: {";
-    for (int i = 0; i < count; i++)
-    {
-        out_st.append(heap.removeMax().toString());
-        if (i != count-1) out_st.append(",");
-    }
-    out_st.append("}");
-    cout << out_st << endl;
+
+    cout << "TEST 1 - heapsort1() n=15 :" << endl;
+    test_heapsort1(15);
+
+    cout << "TEST 2 - heapsort2() n=15 :" << endl;
+    test_heapsort2(15);
+
+    cout << "TEST 3 - heapsort2() n=31 :" << endl;
+    test_heapsort1(31);
+
+    cout << "TEST 4 - heapsort2() n=31 :" << endl;
+    test_heapsort2(31);
 }
