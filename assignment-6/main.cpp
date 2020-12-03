01fffaef01c0120f0e38ff60acf6210922d6e154
hga38 <richard_gao@sfu.ca>
2020-12-02 20:07:24 -0800

    implemented tests

--- assignment-6/main.cpp
+++ assignment-6/main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 void test1()
 {
+    cout << "\t- TEST 1 -\n";
     AVLTree tree = AVLTree();
     const int num_vals = 7;
 
-    cout << "Inserting " << 7 << " values" << endl; 
+    cout << "Inserting " << 7 << " values." << endl; 
 
     for (int i = 0; i < num_vals; ++i)
-        tree.put(i);
+        cout << "\tinserted: " << tree.put(i).key() << endl;
     
     cout << tree;
 
@@ -35,10 +36,39 @@ void test1()
     cout << "Result: " << tree.find(99) << endl;
 }
 
+void test2()
+{
+    cout << "\t- TEST 2 -\n";
+    const int num_vals = 15;
+    int entries[num_vals];
+
+    const int rand_cap = 100;
+
+    cout << "Generating " << num_vals << " values with range 0 - " << rand_cap-1 << endl; 
+    for (int& i : entries)
+        i = rand() % rand_cap;
+
+    cout << "Inserting generated entries." << endl;
+    AVLTree tree = AVLTree();
+    for (int i : entries)
+        cout << "\tinserted: " << tree.put(i) << endl;
+    
+    cout << tree;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << "\t erasing: " << entries[i] << endl;
+        tree.erase(entries[i]);
+    }
+
+    cout << tree;
+}
+
 int main(int argc, char const *argv[])
 {
     srand(time(0));
     test1();
+    test2();
 
     return 0;
 }
